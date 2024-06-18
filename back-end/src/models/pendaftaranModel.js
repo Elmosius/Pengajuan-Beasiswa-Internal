ba38bc0a6dcebaf5a8e0d401d2cf2945d8d62472
Elmosius <2272008@maranatha.ac.id>
2024-06-18 23:47:30 +0700

    updated: pendaftaran model

--- back-end/src/models/pendaftaranModel.js
+++ back-end/src/models/pendaftaranModel.js
@@ -1,8 +1,8 @@
 const dbPool = require("../config/database");
 
 const findPendaftaranById = async (id) => {
-  const query = `SELECT * FROM Pendaftaran WHERE id = ${id}`;
-  return dbPool.execute(query);
+  const query = "SELECT * FROM Pendaftaran WHERE id = ?";
+  return dbPool.execute(query, [id]);
 };
 
 const findAllPendaftaran = async () => {
@@ -11,26 +11,18 @@ const findAllPendaftaran = async () => {
 };
 
 const insertPendaftaran = async (data) => {
-  const query = `INSERT INTO Pendaftaran (id, start_at, end_at, status, created_at, updated_at)
-                  VALUES ('${data.id}', '${data.start_at}', '${data.end_at}', '${data.status}', now(), now())`;
-
-  return dbPool.execute(query);
+  const query = "INSERT INTO Pendaftaran (id, start_at, end_at, status, created_at, updated_at) VALUES (?, ?, ?, ?, now(), now())";
+  return dbPool.execute(query, [data.id, data.start_at, data.end_at, data.status]);
 };
 
 const updatePendaftaran = async (id, data) => {
-  const query = `UPDATE Pendaftaran 
-                 SET start_at = '${data.start_at}',
-                     end_at = '${data.end_at}', 
-                     status = '${data.status}', 
-                     updated_at = now() 
-                  WHERE id = ${id}`;
-
-  return dbPool.execute(query);
+  const query = "UPDATE Pendaftaran SET start_at = ?, end_at = ?, status = ?, updated_at = now() WHERE id = ?";
+  return dbPool.execute(query, [data.start_at, data.end_at, data.status, id]);
 };
 
 const deletePendaftaran = async (id) => {
-  const query = `DELETE FROM Pendaftaran WHERE id = '${id}'`;
-  return dbPool.execute(query);
+  const query = "DELETE FROM Pendaftaran WHERE id = ?";
+  return dbPool.execute(query, [id]);
 };
 
 module.exports = {
