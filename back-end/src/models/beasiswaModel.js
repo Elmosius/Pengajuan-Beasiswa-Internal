36ac3fc00dd16dc62bb3b3af5ba7299d4d90f717
Elmosius <2272008@maranatha.ac.id>
2024-06-18 23:49:56 +0700

    updated: beasiswa model

--- back-end/src/models/beasiswaModel.js
+++ back-end/src/models/beasiswaModel.js
@@ -1,8 +1,8 @@
 const dbPool = require("../config/database");
 
 const findBeasiswaById = async (id) => {
-  const query = `SELECT * FROM Beasiswa WHERE id = ${id}`;
-  return dbPool.execute(query);
+  const query = "SELECT * FROM Beasiswa WHERE id = ?";
+  return dbPool.execute(query, [id]);
 };
 
 const findAllBeasiswa = async () => {
@@ -11,26 +11,18 @@ const findAllBeasiswa = async () => {
 };
 
 const insertBeasiswa = async (data) => {
-  const query = `INSERT INTO Beasiswa (id, nama_beasiswa, deskripsi, periode, created_at, updated_at)
-                  VALUES ('${data.id}', '${data.nama_beasiswa}', '${data.deskripsi}', '${data.periode}', now(), now())`;
-
-  return dbPool.execute(query);
+  const query = "INSERT INTO Beasiswa (id, nama_beasiswa, deskripsi, periode, created_at, updated_at) VALUES (?, ?, ?, ?, now(), now())";
+  return dbPool.execute(query, [data.id, data.nama_beasiswa, data.deskripsi, data.periode]);
 };
 
 const updateBeasiswa = async (id, data) => {
-  const query = `UPDATE Beasiswa 
-                 SET nama_beasiswa = '${data.nama_beasiswa}',
-                     deskripsi = '${data.deskripsi}', 
-                     periode = '${data.periode}', 
-                     updated_at = now() 
-                  WHERE id = ${id}`;
-
-  return dbPool.execute(query);
+  const query = "UPDATE Beasiswa SET nama_beasiswa = ?, deskripsi = ?, periode = ?, updated_at = now() WHERE id = ?";
+  return dbPool.execute(query, [data.nama_beasiswa, data.deskripsi, data.periode, id]);
 };
 
 const deleteBeasiswa = async (id) => {
-  const query = `DELETE FROM Beasiswa WHERE id = '${id}'`;
-  return dbPool.execute(query);
+  const query = "DELETE FROM Beasiswa WHERE id = ?";
+  return dbPool.execute(query, [id]);
 };
 
 module.exports = {
